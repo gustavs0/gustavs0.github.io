02ad6da41adf472cf7e55dbdcdac3a2464047470
GUSTAVO CASTRO <50755275+gustavs0@users.noreply.github.com>
2019-09-17 21:14:39 -0300

    freeway finalizado

---
+++ Feeway/imagens.js
@@ -0,0 +1,23 @@
+let imagensDaEstrada;
+let ImagensDoAtor;
+let ImagensDoAtor2;
+let imagensCarro = [];
+
+let trilhaSonora;
+let somColidiu;
+let somPonto;
+
+function preload(){
+trilhaSonora = loadSound("sons/trilha.mp3");
+somColidiu = loadSound("sons/colidiu.mp3");
+somPonto = loadSound("sons/pontos.wav");
+imagemDaEstrada = loadImage("imagens/estrada.png");
+imagemDoAtor2 = loadImage("imagens/ator-1.png");
+imagemDoAtor = loadImage("imagens/ator-1.png");
+imagensCarro[0] = loadImage("imagens/carro-1.png");
+imagensCarro[1] = loadImage("imagens/carro-2.png");
+imagensCarro[2] = loadImage("imagens/carro-3.png");
+imagensCarro[3] = loadImage("imagens/carro-1.png");
+imagensCarro[4] = loadImage("imagens/carro-2.png");
+imagensCarro[5] = loadImage("imagens/carro-3.png");
+}
